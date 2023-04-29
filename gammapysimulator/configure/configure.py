6bf8b458bc5fdd29d6fb6ee6d8b9a95b79bfe9b6
gabriele.panebianco <gabriele.panebianco@inaf.it>
2023-04-30 00:49:33 +0200

    add YAML reader

--- gammapysimulator/configure/configure.py
+++ gammapysimulator/configure/configure.py
@@ -7,15 +7,31 @@
 #######################################################
 
 from gammapysimulator.tools import logger
-
+import yaml
 class SimulationConfigurator:
     """
     Class that reads a YML file and defines the parameters needed for the simulation.
     """
     
-    def __init__(self, ConfigurationFileName)->None:
+    def __init__(self)->None:
         
         self.log = logger.SimulatorLogger().getLogger()
+
+    
+    def read(self, ConfigurationFileName):
+        """
+        Read the YAML Configuration file into a dictionary.
+        Create the output directory and a log file.
+    
+        Parameters
+        ----------
+        ConfigurationFileName : str
+            Name of the input YAML file.
+        """
+
+        # Load YAML as a dict
+        self.log.info("Loading YAML file...")
+        with open(ConfigurationFileName) as f:
+            configuration = yaml.load(f, Loader = yaml.SafeLoader)
         
-        self.log.info(f"FileName: {ConfigurationFileName}")
-        return None
+        return
--- test/configure/test_configure.py
+++ test/configure/test_configure.py
@@ -8,6 +8,7 @@
 
 import pytest
 from gammapysimulator.configure.configure import SimulationConfigurator
+from gammapysimulator.tools.logger import SimulatorLogger
 
 #######################################################
 # Fixtures
@@ -21,7 +22,23 @@ class TestSimulationConfigurator:
     """Class to test Simulation Configurator"""
     
     def test_init(self):
-        """Test that the class is corectly instantiated"""
-        configurator = SimulationConfigurator("filename")
+        """Test that the class is correctly instantiated"""
         
+        # Instantiate Configurator
+        configurator = SimulationConfigurator()
+        
+        # Assert Class
         assert isinstance(configurator, SimulationConfigurator)
+        assert isinstance(configurator.log, SimulatorLogger)
+
+    def test_init(self, path_configuration_files):
+        """Test that a YAML configuration file is read"""
+        
+        # Instantiate Configurator
+        configurator = SimulationConfigurator()        
+        # Read Configuration file
+        configurator.read(path_configuration_files['configuration'])
+        
+        
+        
+        
