41d15246b5225125fe4c99b5af94f937c582f725
gabriele.panebianco <gabriele.panebianco@inaf.it>
2023-05-17 16:34:16 +0200

    read background from BAK

--- gammapysimulator/scheduler/GBMscheduler.py
+++ gammapysimulator/scheduler/GBMscheduler.py
@@ -6,11 +6,20 @@
 #
 #######################################################
 
+from astropy import units as u
+from astropy.io import fits
+from astropy.table import Table, QTable
 from gammapy.data import GTI
+from gammapy.maps import RegionNDMap, MapAxis, RegionGeom
+from regions import CircleSkyRegion
 
 import numpy as np
+import os
 
+from gammapysimulator.configure.configure import SimulationConfigurator
 from gammapysimulator.scheduler.scheduler import Scheduler
+from gammapysimulator.tools.export import ExportSimulations
+from gammapysimulator.tools import utils
 
 
 class GBMScheduler(Scheduler):
@@ -18,7 +27,7 @@ class GBMScheduler(Scheduler):
     Class that organises Observations and Datasets with Fermi GBM IRFs.
     """
     
-    def __init__(self, configurator) -> None:
+    def __init__(self, configurator : SimulationConfigurator, export : ExportSimulations) -> None:
         """
         Instantiate Scheduler by reading IRFs.
         
@@ -26,10 +35,13 @@ class GBMScheduler(Scheduler):
         ----------
         configurator : gammapysimulator.configure.configure.SimulationConfigurator
             Configurator object.
+        export : gammapysimulator.tools.export.ExportSimulations
+            Export object.
         """
         
         self.conf= configurator
         self.log = configurator.log
+        self.export = export
     
     def LoadIRFs(self):
         """
@@ -40,9 +52,9 @@ class GBMScheduler(Scheduler):
         GTIs = self._define_GTIs()
         
         # Load Background Spectrum
-        try:
+        if "BAK" in self.conf.IRFfilepath.keys():
             background=self._read_Background_from_BAK(self.conf.IRFfilepath['BAK'])
-        except KeyError:
+        else:
             self.log.warning("Key \'BAK\' not found: no background file selected.")
         
         # Load IRFs: Effectiva Area and Energy Dispersion Probability
@@ -77,8 +89,84 @@ class GBMScheduler(Scheduler):
         
         return GTIs
     
-    def _read_Background_from_BAK(self, filepath):
-        raise NotImplementedError
+    def _read_Background_from_BAK(self, Filepath):
+        """
+        Read Background Spectrum from a .bak file.
+        
+        Parameters
+        ----------
+        Filepath : pathlib.Path
+            Path of the background file.
+        
+        Return
+        ------
+        background : gammapy.maps.RegionNDMap
+            Map containing the background spectrum.
+        """
+        # Check file existence
+        self.log.info(f"Load BAK from file: {Filepath}")
+        if not os.path.isfile(Filepath):
+            raise FileNotFoundError(f"File Not Found: {Filepath}")    
+        
+        # Load requested HDUs
+        with fits.open(Filepath) as hdulist:
+            Ebounds = Table.read(hdulist['EBOUNDS' ])
+            Spectrum= Table.read(hdulist['SPECTRUM'])
+
+        # Create the energy axis of the Spectrum and the Geometry
+        EnergyAxis = MapAxis.from_energy_edges(np.append(Ebounds['E_MIN'],
+                                                         Ebounds['E_MAX'][-1]
+                                                         ),
+                                               unit=self.conf.energyUnit
+                                               )
+        Geometry = RegionGeom.create(CircleSkyRegion(self.conf.target, self.conf.RegionRadius),
+                                     axes = [EnergyAxis]
+                                     )
+        
+        # Get the background rates, and scale them according to the simulation integration time
+        if 'RATE' in Spectrum.colnames:
+            RateUnit = u.Unit(f"{Spectrum.meta['TIMEUNIT']}-1")
+            Rates = Spectrum['RATE'] * RateUnit
+        elif 'COUNTS' in Spectrum.colnames:
+            IntegrationTime = 1.0*u.s
+            Rates = Spectrum['COUNTS'].data / IntegrationTime
+            raise NotImplementedError("READ INTEGRATION TIME OF THE COUNTS")
+        else:
+            raise KeyError()
+        
+        # This Map has a different precision wrt to the requested analysis binning,
+        # we must interpolate rates.
+        RatesInterpolated = utils.InterpolateMap(Rates, EnergyAxis.center, self.conf.AxisEnergyReco.center, scale='log')
+        
+        # Plot the old and new Rates
+        self.export.PlotStep([(EnergyAxis.center, Rates, "Rates File"),
+                              (self.conf.AxisEnergyReco.center, RatesInterpolated, "Rates Interpolated")
+                              ],
+                             {'xlabel': f"Energy / {self.conf.AxisEnergyReco.unit}",
+                              'ylabel': f"Rates / ({RatesInterpolated.unit})",
+                              'xscale' : "log",
+                              'yscale' : "log",
+                              'title' : "Background Rates IRF",
+                              'figurename' : "irfs/ratesbak"
+                              }
+                             )
+        
+        # Get the Counts by Multiplying Rates for the simulation integration time.
+        Counts = (RatesInterpolated * self.conf.timeReso).to('') 
+        
+        # Create the RegionNDMap with Counts and plot
+        CountsMap = RegionNDMap.from_geom(self.conf.geometry, data=Counts.value, unit=Counts.unit, meta=Spectrum.meta)
+        self.export.PlotStep([(self.conf.AxisEnergyReco.center, Counts, "Background")],
+                             {'xlabel': f"Energy / {self.conf.AxisEnergyReco.unit}",
+                              'ylabel': f"Counts",
+                              'xscale' : "log",
+                              'yscale' : "log",
+                              'title' : f"Background Counts IRF [{self.conf.timeReso}]",
+                              'figurename' : "irfs/countsbak"
+                              }
+                             )     
+        
+        return CountsMap
     
     def _read_Exposure_from_RSP(self, filepath):
         raise NotImplementedError
--- gammapysimulator/tools/utils.py
+++ gammapysimulator/tools/utils.py
@@ -6,10 +6,13 @@
 #
 #######################################################
 
+import matplotlib.pyplot as plt
+import numpy as np
 import os
 import shutil
 
 from gammapy.utils.table import table_from_row_data
+from scipy import interpolate
 
 def get_absolute_path(path):
     """Get absolute path from either $ENV or relative path.
@@ -76,3 +79,53 @@ def info_table(datasets, cumulative=False):
             rows.append(row)
 
         return table_from_row_data(rows=rows)
+    
+
+def InterpolateMap(OldValues, OldAxis, NewAxis, scale='lin', kind='linear'):
+    """
+    Interpolate differential quantities to find their values in a new axis.
+    
+    Parameters
+    ----------
+    OldValues : astropy.Quantity
+        Quantity evaluated at old axis.
+    OldAxis, NewAxis : astropy.Quantity
+        Old and New axis where to evaluate the quantity values.
+    scale : str
+        Either 'lin' or 'log' to evaluate quantities at lin or log axes bins.
+    kind : str
+        Argument passed to scipy.interpolate.interp1d
+        
+    Returns
+    -------
+    Newvalues : astropy.Quantity
+        Quantity evaluated at new axis.
+    """
+    
+    # Check New Bounds are within Old Bounds
+    if NewAxis[0 ] < OldAxis[0 ]:
+        raise ValueError(f"Requested function evaluation at {NewAxis[0 ]}. The minimum is {OldAxis[0 ]}.")
+    if NewAxis[-1] > OldAxis[-1]:
+        raise ValueError(f"Requested function evaluation at {NewAxis[-1]}. The maximum is {OldAxis[-1]}.")
+    
+    # Function evaluation at linear or logarithmic bins
+    if scale=="lin":
+        xold = OldAxis.value
+        xnew = NewAxis.value
+    elif scale=="log":
+        xold = np.log10(OldAxis.value)
+        xnew = np.log10(NewAxis.value)
+    else:
+        raise ValueError("Scale must be either lin or log.")
+    yold = OldValues.value
+    
+    # Perform interpolation
+    f = interpolate.interp1d(xold, yold, kind=kind)
+    
+    # Define new values
+    ynew = f(xnew)
+    
+    # Add unit
+    NewValues = ynew * OldValues.unit
+    
+    return NewValues
